6f84a2d0ccf4adac62e577515824d641baefea9a
BlessingLE <lacheevandipb@gmail.com>
2023-06-18 14:41:52 +0200

    Creating basic Hello world page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+//Typescript file
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [],
-  template: `<h1>Default</h1>`,
+  template: `<h1>Hello World!</h1>`, //changes the text displayed on page
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+  title = 'homes';
 }
