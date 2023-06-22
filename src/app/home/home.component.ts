b44c7c6ca901b31f60cc3dd3a8749acc48379dcf
BlessingLE <lacheevandipb@gmail.com>
2023-06-22 17:16:55 +0200

    Displayed all housing locations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,28 +17,126 @@ import { HousingLocation } from '../housing-location/housing-location.component'
   </section>
 
   /*<section class="results">
-    <app-housing-location [housinglocation]="housing"></app-housing-location>
+  <app-housing-location
+  *ngFor="let housingLocation of housingLocationList"
+  [housinglocation]="housingLocation">
+</app-housing-location>
   </section>
 `, 
 //The first section creates a search bar that filters by city and adds a button to search
+
 //The second section adds the housing location to the home template
-/*In the second part, [housingLocation]="housingLocation" is called property binding. It is 
+/*In the second part, [housingLocation]="housing" is called property binding. It is 
 in the format [property attribute used to create an instance of the actual property] = "propery attribute containing the actual data values". "value" is thr name of the property in the specific component. 
 property binding allows you to connet a variable to an input() in angular*/
+
+/*Now we replaced the second part with <app-housing-location *ngFor= let housinglocation of housingLocationList" [housinglocation]="housingLocation"></app-housing-location>
+This allows us to use the ngFor directive which performs thwe same functions as a for loop. Before we didnt need it because only had one housing location. But now we have multiple and will need to loop through 
+all of them.*/
 styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent {
-  /*add housinglocation of type housing location to 
-  ensure that the data matches the description of the interface.*/
-  housing: HousingLocation = {
-    id:9999,
-    name: 'Test Home',
-    city: 'Test City',
-    state: 'ST',
-    photo: 'assets/example-house.jpg',
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };  //initialising the variables or the interface with test data
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: '/assets/brandon-griggs-wR11KBaB86U-unsplash.jpg',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: '/assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 3,
+      name: 'Homesteady Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Gary',
+      state: 'IN',
+      photo: '/assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 5,
+      name: 'Hopeful Apartment Group',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 6,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
+      availableUnits: 5,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 7,
+      name: 'Hopeful Housing Solutions',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/r-architecture-GGupkreKwxA-unsplash.jpg',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 8,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/saru-robert-9rP3mxf8qWI-unsplash.jpg',
+      availableUnits: 10,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 9,
+      name: 'Capital Safe Towns',
+      city: 'Portland',
+      state: 'OR',
+      photo: '/assets/webaliser-_TPTXZd9mOo-unsplash.jpg',
+      availableUnits: 6,
+      wifi: true,
+      laundry: true
+    }
+  ]; //above is an array of multiple housing locations which we will display on the web page
 }
